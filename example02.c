910dd869a9ecec68e39b97b0f2545001cbbae210
Maukrack <majiqtresi@gmail.com>
2024-01-09 14:09:04 -0600

    TypeCasting update

--- example02.c
+++ example02.c
@@ -12,5 +12,8 @@ int main(){
     p0 = (char *)p; //typecasting
     printf("size of char is %d bytes \n", sizeof(char));
     printf("Address = %d, value %d \n", p0, *p0);
+    printf("Address = %d, value %d \n", p0+1, *(p0+1));
+    // 1025
+    /// 00000000 00000000 00000100 00000001
     return 0;
 }
