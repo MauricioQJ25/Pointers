a113d325689674ae3893a64c08ad00a9a572a8eb
Maukrack <majiqtresi@gmail.com>
2024-01-09 14:03:09 -0600

    Aritmethic Pointer

--- example01.c
+++ example01.c
@@ -11,6 +11,9 @@ int main(){
     *p = b; 
     printf("Address of P is %d\n", p);
     printf("Value at p is %d\n", *p);
+    printf("Size of  integer is %d bytes \n", sizeof(int));
+    printf("Address p+1 is %d \n", p+2);
+
     return 0;
 }
     
