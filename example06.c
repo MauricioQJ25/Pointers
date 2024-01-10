bba57be5a6f6af3bdcc454ba7ebe59666bb1b3ed
Maukrack <majiqtresi@gmail.com>
2024-01-09 21:22:28 -0600

    Updating example Characters arrays and pointers

--- example06.c
+++ example06.c
@@ -14,6 +14,8 @@ int main(){
     char Name[5] = "Mau";
     print(Name);
     char *Name2 = "Mau2";
+    //Not allowed, in this case the previous line is going to be a constant
+    //Name2[0] = 'N';
     print(Name2);
     return 0; 
 }
