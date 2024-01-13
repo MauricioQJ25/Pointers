acd9fb9ee865f602be03bb3f255592a1edd8838c
Maukrack <majiqtresi@gmail.com>
2024-01-12 18:08:53 -0600

    example07.c

--- example07.c
+++ example07.c
@@ -9,7 +9,7 @@ int main(){
                         {{5, 6}, { 7, 8}},
                         {{9,10}, {11,12}}
                     };
-    printf("Size of int %ld", sizeof(int));
+    printf("Size of int %ld\n", sizeof(int));
     printf("%d %d %d %d\n", C, *C, C[0], &C[0][0]);
-    printf("%d\n", *(C[0][0]+1));
+    printf("%d\n", *(C[0][4]));
 }
